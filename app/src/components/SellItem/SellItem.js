ff8236bd385d4dcdbff573b414a62b5d40c6d580
Alexander Perala <alexander.perala@hotmail.com>
2021-02-08 19:24:38 +0100

    Add "sell item page"

---
+++ app/src/components/SellItem/SellItem.js
@@ -0,0 +1,12 @@
+import Header from "../Header/Header";
+
+const SellItem = () => {
+  return (
+    <div>
+      <Header />
+      <h4>This is the "sell-item page"</h4>
+    </div>
+  );
+};
+
+export default SellItem;
