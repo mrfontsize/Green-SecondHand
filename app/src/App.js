16e8630fe773188fb294e42f66fb1a2817ff7a6c
Alexander Perala <alexander.perala@hotmail.com>
2021-02-08 19:25:32 +0100

    Add new "Route to - Sellitem-page"

--- app/src/App.js
+++ app/src/App.js
@@ -1,17 +1,19 @@
-import { 
-  BrowserRouter as Router, 
-  Switch, 
-  Route } from "react-router-dom";
-import Home from "./components/Home";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import SellItem from "./components/SellItem/SellItem";
+import "./style.css";
 
 function App() {
   return (
     <Router>
       <Switch>
 
-
         <Route exact path="/">
           <Home />
+        </Route>
+
+        <Route path="/SellItem">
+          <SellItem />
         </Route>
 
 
